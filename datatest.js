94e85a34a542fc47366c4d1acd64ab9962618284
TsukemonoGit <mine43try@outlook.com>
2024-02-06 08:11:42 +0900

    check url

--- datatest.js
+++ datatest.js
@@ -6,13 +6,19 @@ const testData = JSON.parse(await readFile(`./iroiro.json`));
 // テスト対象の関数
 function checkForDuplicateTitles(data) {
   const titles = new Set(); // 重複をチェックするためのセット
+  const urls = new Set();//urlの重複もチェックする
 
   for (const key in data) {
     const item = data[key];
     if (titles.has(item.title)) {
-      return item.title; // 重複が見つかった場合はtitleを返す
+      return `title: ${item.title}`; // 重複が見つかった場合はtitleを返す
+    }
+    if (urls.has(item.url)) {
+      return `url: ${item.url}`; // 重複が見つかった場合はtitleを返す
     }
     titles.add(item.title); // セットにタイトルを追加
+    urls.add(item.url);
+
   }
 
 
@@ -23,7 +29,7 @@ function checkForDuplicateTitles(data) {
 const hasDuplicates = checkForDuplicateTitles(testData);
 // 結果の出力
 if (hasDuplicates) {
-  console.log(`Error! Duplicate title [ ${hasDuplicates} ]`);
+  console.log(`Error! Duplicate ${hasDuplicates}`);
 } else {
   console.log("OK!");
 }
